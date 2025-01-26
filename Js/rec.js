54075eab705b06d558fcc62db6d311eef61f970d
Dauken Nurgaziyev <d.nurgaziyev@mycar.kz>
2025-01-26 17:07:21 +0600

    adptive update

--- Js/rec.js
+++ Js/rec.js
@@ -1,5 +1,5 @@
 const wrapperCatalog = document.querySelector(".wrapper-catalog");
 const element = document.createElement('h1');
-element.textContent = "mamascript";
+element.textContent = "abuhalka pidor";
 element.style.color = "red";
 wrapperCatalog.append(element);
